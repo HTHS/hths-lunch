289fa72e9d6c96b4546f3373d23d64796d973788
Ilan Biala <ilan.biala@gmail.com>
2014-11-13 23:33:28 -0500

    Cleaner server init file

--- server.js
+++ server.js
@@ -7,15 +7,17 @@ var chalk = require('chalk'),
 	express = require('./config/express'),
 	mongoose = require('./config/mongoose');
 
-mongoose.connect(function(db) {
-	var app = express(db);
+mongoose
+	.connect()
+	.then(function(db) {
+		var app = express(db);
 
-	// Bootstrap passport config
-	require('./config/passport')();
+		// Bootstrap passport config
+		require('./config/passport')();
 
-	// Start the app by listening on <port>
-	exports = module.exports = app.listen(config.port);
+		// Logging initialization
+		console.log(chalk.green('Application started on port ' + config.port));
 
-	// Logging initialization
-	console.log(chalk.green('Application started on port ' + config.port));
-});
+		// Start the app by listening on <port>
+		app.listen(config.port);
+	});
