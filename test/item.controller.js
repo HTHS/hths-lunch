6721a8b2e97e8f8fada21a1fe1b1dc1641bee35a
Ilan Biala <ilan.biala@gmail.com>
2014-11-13 23:30:24 -0500

    Separate model and controller test files

---
+++ test/item.controller.js
@@ -0,0 +1,36 @@
+/**
+ * Module dependencies
+ */
+var should = require('should'),
+	mongoose = require('mongoose'),
+	request = require('supertest'),
+	Item = mongoose.model('Item'),
+	item = require('../app/controllers/item');
+
+/**
+ * Unit tests
+ */
+describe('Item controller unit tests:', function() {
+	before(function(done) {
+		done();
+	});
+
+	describe('Creating an Item', function() {
+		it('should create and save an Item', function(done) {
+			// request(server)
+			// 	.get('/path')
+			// 	.expect('Content-Type', /json/)
+			// 	.expect(200, done);
+			done();
+		});
+
+		it('fails to create an Item with missing field', function(done) {
+			done();
+		});
+	});
+
+	after(function(done) {
+		Item.remove().exec();
+		done();
+	});
+});
