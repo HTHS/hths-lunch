bcdc7f3a34e0c78a4a89a2caf9c290015f4b9974
Ilan Biala <ilan.biala@gmail.com>
2014-11-20 00:24:53 -0500

    Better gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,35 +4,21 @@ var gulp = require('gulp'),
 	rename = require('gulp-rename'),
 	sass = require('gulp-sass'),
 	mocha = require('gulp-mocha'),
-	istanbul = require('gulp-istanbul');
+	istanbul = require('gulp-istanbul'),
+	sourcemaps = require('gulp-sourcemaps'),
+	uglify = require('gulp-uglify'),
+	plato = require('gulp-plato');
 
-/**
- * Helpers
- */
+/***********
+ * Helpers *
+ ***********/
 gulp.task('env:test', function() {
 	process.env.NODE_ENV = 'test';
 });
 
-gulp.task('default', function() {
-	// place code for your default task here
-});
-
-gulp.task('concat', function() {
-	del.sync('public/app.js');
-
-	return gulp.src([
-			'public/lib/jquery/dist/jquery.min.js',
-			'public/lib/angular/angular.min.js',
-			'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-			'public/lib/angular-resource/angular-resource.min.js',
-			'public/lib/foundation/js/vendor/modernizr.js',
-			'public/lib/foundation/js/foundation.min.js',
-			'public/modules/**/*.js'
-		])
-		.pipe(concat('app.js'))
-		.pipe(gulp.dest('public/'));
-});
-
+/*******
+ * CSS *
+ *******/
 gulp.task('sass', function() {
 	gulp.src([
 			'public/sass/**/*.scss'
@@ -50,26 +36,33 @@ gulp.task('sass', function() {
 		.pipe(gulp.dest('public/modules/'));
 });
 
-gulp.task('watch', function() {
-	var jsWatcher = gulp.watch('**/*.js', {
-		cwd: 'public/'
-	}, ['concat']);
-	jsWatcher.on('change', function(event) {
-		console.log('File %s was %s, running tasks...', event.path, event.type);
-	});
+/**************
+ * JavaScript *
+ **************/
+gulp.task('concat', function() {
+	del.sync('public/app.js');
 
-	var sassWatcher = gulp.watch('**/*.scss', {
-		cwd: 'public/'
-	}, ['sass']);
-	sassWatcher.on('change', function(event) {
-		// event.path = event.path.replace('sass', 'styles');
-		console.log('File %s was %s, running tasks...', event.path, event.type);
-	});
+	return gulp.src([
+			'public/lib/jquery/dist/jquery.min.js',
+			'public/lib/angular/angular.min.js',
+			'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+			'public/lib/angular-resource/angular-resource.min.js',
+			'public/lib/foundation/js/vendor/modernizr.js',
+			'public/lib/foundation/js/foundation.min.js',
+			'public/modules/**/*.js'
+		])
+		.pipe(sourcemaps.init())
+		// .pipe(uglify())
+		.pipe(concat('app.js'))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('public/'));
 });
 
+/*********
+ * Tests *
+ *********/
 gulp.task('mocha', ['env:test'], function() {
 	var mongoose = require('./config/mongoose');
-
 
 	gulp.src(['app/**/*.js', 'config/**/*.js', 'public/modules/**/*.js'])
 		.pipe(istanbul())
@@ -107,10 +100,52 @@ gulp.task('mocha', ['env:test'], function() {
 		});
 });
 
+/************
+ * Analysis *
+ ************/
+gulp.task('plato', function() {
+	return gulp.src(['app/**/*.js', 'config/**/*.js', 'public/modules/**/*.js'])
+		.pipe(plato('report', {
+			jshint: {},
+			complexity: {
+				trycatch: true
+			}
+		}));
+});
+
+gulp.task('analysis', ['plato']);
+
+/*********************
+ * Development tasks *
+ *********************/
+gulp.task('watch', function() {
+	var jsWatcher = gulp.watch('public/**/*.js', {}, ['concat']);
+	jsWatcher.on('change', function(event) {
+		console.log('File %s was %s, running tasks...', event.path, event.type);
+	});
+
+	var sassWatcher = gulp.watch('public/**/*.scss', {}, ['sass']);
+	sassWatcher.on('change', function(event) {
+		// event.path = event.path.replace('sass', 'styles');
+		console.log('File %s was %s, running tasks...', event.path, event.type);
+	});
+});
+
+gulp.task('dev', function() {
+
+});
+
+/*******************
+ * Composite tasks *
+ *******************/
 gulp.task('test', ['mocha'], function() {
 
 });
 
 gulp.task('build', ['sass', 'concat'], function() {
 
+});
+
+gulp.task('default', function() {
+	// place code for your default task here
 });
