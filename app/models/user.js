e18d786a1291b24ffee4f085879cdd4672dc441d
Ilan Biala <ilan.biala@gmail.com>
2014-11-23 12:52:19 -0500

    Added some idea of OAuth admins

--- app/models/user.js
+++ app/models/user.js
@@ -2,48 +2,52 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
+  Schema = mongoose.Schema;
 
 /**
  * User Schema
  */
 var UserSchema = new Schema({
-	firstName: {
-		type: String,
-		trim: true,
-		default: ''
-	},
-	lastName: {
-		type: String,
-		trim: true,
-		default: ''
-	},
-	displayName: {
-		type: String,
-		trim: true
-	},
-	email: {
-		type: String,
-		trim: true
-	},
-	provider: {
-		type: String,
-		required: 'Provider is required'
-	},
-	providerData: {},
-	additionalProvidersData: {},
-	updated: {
-		type: Date
-	},
-	created: {
-		type: Date,
-		default: Date.now
-	},
-	orderHistory: [{
-		type: Schema.Types.ObjectId,
-		ref: 'Order',
-		required: true
-	}]
+  firstName: {
+    type: String,
+    trim: true,
+    default: ''
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    default: ''
+  },
+  displayName: {
+    type: String,
+    trim: true
+  },
+  email: {
+    type: String,
+    trim: true
+  },
+  // isAdmin: {
+  // 	type: Boolean,
+  // 	default: false
+  // },
+  provider: {
+    type: String,
+    required: 'Provider is required'
+  },
+  providerData: {},
+  additionalProvidersData: {},
+  updated: {
+    type: Date
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  orderHistory: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Order',
+    required: true
+  }]
 });
 
 mongoose.model('User', UserSchema);
