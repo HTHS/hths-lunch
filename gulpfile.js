21b60d74874fcb05dab535ed3cadb03c8bf2caf7
Ilan Biala <ilan.biala@gmail.com>
2014-12-06 01:16:27 -0500

    Better gulp.watch for JS

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('concat', function() {
 		.pipe(gulp.dest('public/'))
 		.pipe(rename('app.min.js'))
 		.pipe(uglify())
-		.pipe(sourcemaps.write('./'))
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('public/'));
 });
 
@@ -117,7 +117,7 @@ gulp.task('analysis', ['plato']);
  *********************/
 gulp.task('watch', function() {
 	gulp
-		.watch('public/**/*.js', {}, ['concat'])
+		.watch(['public/**/*.js', '!public/*.js'], {}, ['concat'])
 		.on('change', function(event) {
 			console.log('File %s was %s, running tasks...', event.path, event.type);
 		});
