95a7478846dd1451e00f9bd6cd4013a1de9c17d5
Ilan Biala <ilan.biala@gmail.com>
2014-12-07 04:30:32 -0500

    Updated update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,6 +3,7 @@
 git checkout .;
 git pull;
 npm install;
+bower --allow-root install;
 gulp build;
 forever restartall;
 echo "Updated to latest version, rebuilt dependencies and scripts, and restarted server.";
