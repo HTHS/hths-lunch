7d2eba843a2057a06b579ab0cddbaae60b377110
Ilan Biala <ilan.biala@gmail.com>
2015-01-04 02:58:50 -0500

    Cleanup

--- server.js
+++ server.js
@@ -2,46 +2,46 @@
  * Module dependencies.
  */
 var chalk = require('chalk'),
-	_ = require('lodash'),
-	config = require('./config/config'),
-	express = require('./config/express'),
-	mongoose = require('./config/mongoose');
+  _ = require('lodash'),
+  config = require('./config/config'),
+  express = require('./config/express'),
+  mongoose = require('./config/mongoose');
 
 exports.start = function() {
-	mongoose
-		.connect()
-		.then(function(db) {
-			var options = require('./config/options');
+  mongoose
+    .connect()
+    .then(function(db) {
+      var options = require('./config/options');
 
-			options.then(function(opts) {
-				var options = opts.reduce(function(prevObj, currentObj) {
-					return _.extend(prevObj, currentObj);
-				});
+      options.then(function(opts) {
+        var options = opts.reduce(function(prevObj, currentObj) {
+          return _.extend(prevObj, currentObj);
+        });
 
-				var app = express(db, options);
+        var app = express(db, options);
 
-				// Bootstrap passport config
-				require('./config/passport')();
+        // Bootstrap passport config
+        require('./config/passport')();
 
-				// Logging the initialization details
-				console.log(chalk.green('%s application started.'), config.app.title);
-				console.log(chalk.green('Environment: %s'), process.env.NODE_ENV);
-				console.log(chalk.green('Port: %d'), config.port);
-				console.log(chalk.green('Database: %s'), config.db);
+        // Logging the initialization details
+        console.log(chalk.green('%s application started.'), config.app.title);
+        console.log(chalk.green('Environment: %s'), process.env.NODE_ENV);
+        console.log(chalk.green('Port: %d'), config.port);
+        console.log(chalk.green('Database: %s'), config.db);
 
-				// Start the app by listening on <port>
-				exports.server = app.listen(config.port);
-			});
-		});
+        // Start the app by listening on <port>
+        exports.server = app.listen(config.port);
+      });
+    });
 };
 
 exports.kill = function() {
-	exports.server.close();
-	mongoose.disconnect();
+  exports.server.close();
+  mongoose.disconnect();
 
 	console.log(chalk.red('Application killed. Port %s now available.'), config.port);
 
-	exports.server = null;
+  exports.server = null;
 };
 
 exports.start();
