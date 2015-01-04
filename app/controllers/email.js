4d2b62142817352da172f31b2a3d5f819ee85b6e
Ilan Biala <ilan.biala@gmail.com>
2015-01-03 19:04:13 -0500

    Modularized emails

--- app/controllers/email.js
+++ app/controllers/email.js
@@ -1,7 +1,37 @@
 /**
  * Module dependencies
  */
-var nodemailer = require('nodemailer'),
+var _ = require('lodash'),
+	nodemailer = require('nodemailer'),
+	Promise = require('bluebird'),
 	config = require('../../config/config');
 
 var transporter = nodemailer.createTransport(config.mailer.options);
+
+/**
+ * Email constructor
+ * @param {Object}  email   email object
+ */
+function Email(email) {
+	this.email = _.extend({
+		from: config.mailer.from
+	}, email);
+	this.isSent = false;
+}
+
+Email.prototype.send = function() {
+	var p = Promise.defer();
+
+	transporter.sendMail(this.email, function(err, info) {
+		if (err) {
+			p.reject(err);
+		} else {
+			this.isSent = true;
+			p.resolve(info);
+		}
+	});
+
+	return p.promise;
+};
+
+module.exports = Email;
