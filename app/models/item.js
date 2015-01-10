7058ff8598bf73625e09b99bc8b2b7eb2bd4fe68
Ilan Biala <ilan.biala@gmail.com>
2015-01-10 01:27:36 -0500

    Add field to item model

--- app/models/item.js
+++ app/models/item.js
@@ -2,32 +2,36 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
+  Schema = mongoose.Schema;
 
 /**
  * Item Schema
  */
 var ItemSchema = new Schema({
-	title: {
-		type: String,
-		required: true
-	},
-	description: {
-		type: String,
-		required: true
-	},
-	price: {
-		type: Number,
-		required: true
-	},
-	active: {
-		type: Boolean,
-		default: true
-	},
-	numberOrdered: {
-		type: Number,
-		default: 0 
-	}
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  active: {
+    type: Boolean,
+    default: true
+  },
+  numberOrdered: {
+    type: Number,
+    default: 0
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 mongoose.model('Item', ItemSchema);
