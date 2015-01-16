1f7f486f8977e19ace1436c33ca82c93079aae96
Ilan Biala <ilan.biala@gmail.com>
2015-01-16 17:05:29 -0500

    Formatting

--- public/modules/panel/directives.js
+++ public/modules/panel/directives.js
@@ -1,53 +1,53 @@
 angular.module('hthsLunch.panel')
-  .directive('barGraph', [function() {
-    return {
-      restrict: 'E',
-      scope: {
-        data: '='
-      },
-      link: function($scope, element, attrs) {
-        var data = {
-          // A labels array that can contain any sort of values
-          labels: $scope.data.labels,
-          // Our series array that contains series objects or in this case series data arrays
-          series: [$scope.data.data]
-        };
+	.directive('barGraph', [function() {
+		return {
+			restrict: 'E',
+			scope: {
+				data: '='
+			},
+			link: function($scope, element, attrs) {
+				var data = {
+					// A labels array that can contain any sort of values
+					labels: $scope.data.labels,
+					// Our series array that contains series objects or in this case series data arrays
+					series: [$scope.data.data]
+				};
 
-        var options = {};
+				var options = {};
 
-        if (attrs.gWidth && attrs.gHeight) {
-          options.width = attrs.gWidth;
-          options.height = attrs.gHeight;
-        }
+				if (attrs.gWidth && attrs.gHeight) {
+					options.width = attrs.gWidth;
+					options.height = attrs.gHeight;
+				}
 
-        $scope.graph = new Chartist.Bar(element[0], data, options);
-      }
-    };
-  }])
-  .directive('lineChart', [function() {
-    return {
-      restrict: 'E',
-      scope: {
-        data: '='
-      },
-      link: function($scope, element, attrs) {
-        var data = {
-          // A labels array that can contain any sort of values
-          labels: $scope.data.labels,
-          // Our series array that contains series objects or in this case series data arrays
-          series: [$scope.data.data]
-        };
+				$scope.graph = new Chartist.Bar(element[0], data, options);
+			}
+		};
+	}])
+	.directive('lineChart', [function() {
+		return {
+			restrict: 'E',
+			scope: {
+				data: '='
+			},
+			link: function($scope, element, attrs) {
+				var data = {
+					// A labels array that can contain any sort of values
+					labels: $scope.data.labels,
+					// Our series array that contains series objects or in this case series data arrays
+					series: [$scope.data.data]
+				};
 
-        var options = {
-          lineSmooth: false
-        };
+				var options = {
+					lineSmooth: false
+				};
 
-        if (attrs.gWidth && attrs.gHeight) {
-          options.width = attrs.gWidth;
-          options.height = attrs.gHeight;
-        }
+				if (attrs.gWidth && attrs.gHeight) {
+					options.width = attrs.gWidth;
+					options.height = attrs.gHeight;
+				}
 
-        $scope.graph = new Chartist.Line(element[0], data, options);
-      }
-    };
-  }]);
+				$scope.graph = new Chartist.Line(element[0], data, options);
+			}
+		};
+	}]);
