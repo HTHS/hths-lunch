a5cf2dfc4a564e4231057545dcc4d426e8fd56c2
Ilan Biala <ilan.biala@gmail.com>
2015-01-15 22:50:53 -0500

    Start of database service

--- public/modules/core/services/database-service.js
+++ public/modules/core/services/database-service.js
@@ -1,10 +1,21 @@
-angular.module('hthsLunch.core.databaseService', ['ngResource'])
-	.factory('Item', ['$resource', function($resource) {
-		return $resource('/api/items/:itemId', {
-			itemId: '@_id'
-		}, {
-			update: {
-				method: 'PUT'
-			}
+angular.module('hthsLunch.core.databaseService', ['hthsLunch.core.itemService', 'hthsLunch.core.orderService', 'hthsLunch.core.userService'])
+	.factory('Database', ['$q', 'Item', 'Order', 'User', function($q, Item, Order, User) {
+		var service = {};
+
+		var me;
+
+		var meQuery = User.me().$promise;
+		meQuery.then(function(user) {
+			me = user;
 		});
+
+		service.getMe = function() {
+			return me;
+		};
+
+		service.fetchAll = function() {
+			return $q.all([meQuery]);
+		};
+
+		return service;
 	}]);
