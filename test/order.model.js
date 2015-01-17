39ae6d4871b8af92ba7ae9d978db3a178988d117
Ilan Biala <ilan.biala@gmail.com>
2015-01-16 23:52:13 -0500

    Fix tests

--- test/order.model.js
+++ test/order.model.js
@@ -2,9 +2,9 @@
  * Module dependencies
  */
 var should = require('should'),
-	mongoose = require('mongoose'),
-	Item = mongoose.model('Item'),
-	Order = mongoose.model('Order');
+  mongoose = require('mongoose'),
+  Item = mongoose.model('Item'),
+  Order = mongoose.model('Order');
 
 var item1, item2;
 
@@ -12,68 +12,66 @@ var item1, item2;
  * Unit tests
  */
 describe('Order model unit tests:', function() {
-	before(function(done) {
-		item1 = new Item({
-			title: 'A delicious test item',
-			description: 'An item description',
-			price: 4.5,
-			active: true
-		});
+  before(function(done) {
+    item1 = new Item({
+      title: 'A delicious test item',
+      description: 'An item description',
+      price: 4.5,
+      active: true
+    });
 
-		item2 = new Item({
-			title: 'Another delicious test item',
-			description: 'Another item description',
-			price: 5.25,
-			active: true
-		});
+    item2 = new Item({
+      title: 'Another delicious test item',
+      description: 'Another item description',
+      price: 5.25,
+      active: true
+    });
 
-		done();
-	});
+    done();
+  });
 
-	describe('Creating an Order', function() {
-		it('should create and save an order', function(done) {
-			var order = new Order({
-				customer: 'Test customer',
-				items: [
-					item1._id,
-					item2._id
-				],
-				quantity: [
-					2,
-					1
-				],
-				total: 14.25,
-				timestamp: new Date()
-			});
+  describe('Creating an Order', function() {
+    it('should create and save an order', function(done) {
+      var order = new Order({
+        customer: 'Test customer',
+        items: [
+          item1._id,
+          item2._id
+        ],
+        quantity: [
+          2,
+          1
+        ],
+        total: 14.25
+      });
 
-			order.save(done);
-		});
+      order.save(done);
+    });
 
-		it('fails to create an Order with missing field', function(done) {
-			var order = new Order({
-				customer: 'Test customer',
-				items: [
-					item1._id,
-					item2._id
-				],
-				quantity: [
-					2,
-					1
-				],
-				// total: 14.25, missing field
-				timestamp: new Date()
-			});
+    it('fails to create an Order with missing field', function(done) {
+      var order = new Order({
+        customer: 'Test customer',
+        items: [
+          item1._id,
+          item2._id
+        ],
+        quantity: [
+          2,
+          1
+        ],
+        // total: 14.25, missing field
+      });
 
-			order.save(function(err) {
-				(err !== null).should.be.true;
-				done();
-			});
+      order.save(function(err) {
+        (err !== null).should.be.true;
+        done();
+      });
 
-		});
-	});
+    });
+  });
 
-	after(function(done) {
-		Order.remove().exec();
-		done();
-	});
+  after(function(done) {
+    Order.remove().exec();
+    done();
+  });
 });
