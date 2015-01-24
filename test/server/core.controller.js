9ebaca7313c2cebf83649f1c055b2f68d7fe3358
Ilan Biala <ilan.biala@gmail.com>
2015-01-23 23:24:39 -0500

    Cleaner test files

--- test/server/core.controller.js
+++ test/server/core.controller.js
@@ -3,39 +3,37 @@
  */
 
 var should = require('should'),
-  request = require('supertest');
+	request = require('supertest')('http://localhost:3001');
 
 /**
  * Unit tests
  */
 describe('Core controller unit tests:', function() {
-  before(function(done) {
-    request = request('http://localhost:3001');
+	before(function(done) {
+		done();
+	});
 
-    done();
-  });
+	describe('Loading the homepage', function() {
+		it('should return 200 from GET /', function(done) {
+			request
+				.get('/')
+				.expect(302)
+				.end(function(err, res) {
+					request
+						.get('/')
+						.expect(200, done);
+				});
+		});
 
-  describe('Loading the homepage', function() {
-    it('should return 200 from GET /', function(done) {
-      request
-        .get('/')
-        .expect(302)
-        .end(function(err, res) {
-          request
-            .get('/')
-            .expect(200, done);
-        });
-    });
+		it('should return an HTML page', function(done) {
+			request
+				.get('/')
+				.expect('Content-Type', /html/)
+				.expect(200, done);
+		});
+	});
 
-    it('should return an HTML page', function(done) {
-      request
-        .get('/')
-        .expect('Content-Type', /html/)
-        .expect(200, done);
-    });
-  });
-
-  after(function(done) {
-    done();
-  });
+	after(function(done) {
+		done();
+	});
 });
