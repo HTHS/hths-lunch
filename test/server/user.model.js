9ecbbcd2f623994558668c9ef058af441bb25720
Ilan Biala <ilan.biala@gmail.com>
2015-01-23 23:17:23 -0500

    Better model assertions in tests

--- test/server/user.model.js
+++ test/server/user.model.js
@@ -2,48 +2,50 @@
  * Module dependencies
  */
 var should = require('should'),
-	mongoose = require('mongoose'),
-	User = mongoose.model('User');
+  mongoose = require('mongoose'),
+  User = mongoose.model('User');
 
 /**
  * Unit tests
  */
 describe('User model unit tests:', function() {
-	before(function(done) {
-		done();
-	});
+  before(function(done) {
+    done();
+  });
 
-	describe('Creating an User', function() {
-		it('should create and save an User', function(done) {
-			var user = new User({
-				firstName: 'Test',
-				lastName: 'User',
-				displayName: 'Test User',
-				email: 'testuser@gmail.com',
-				provider: 'local'
-			});
+  describe('Creating an User', function() {
+    it('should create and save an User', function(done) {
+      var user = new User({
+        firstName: 'Test',
+        lastName: 'User',
+        displayName: 'Test User',
+        email: 'testuser@gmail.com',
+        provider: 'local'
+      });
 
-			user.save(done);
-		});
+      user.save(done);
+    });
 
-		it('fails to create an User with missing field', function(done) {
-			var user = new User({
-				firstName: 'Test',
-				lastName: 'User',
-				displayName: 'Test User',
-				email: null, // missing
-				provider: 'local'
-			});
+    it('fails to create an User with missing field', function(done) {
+      var user = new User({
+        firstName: 'Test',
+        lastName: 'User',
+        displayName: 'Test User',
+        // missing email
+        provider: 'local'
+      });
 
-			user.save(function(err) {
-				(err !== null).should.be.true;
-				done();
-			});
-		});
-	});
+      user.save(function(err) {
+        (err !== null).should.be.true;
+        err.message.should.equal('Validation failed');
+        err.errors.email.message.should.equal('Path `email` is required.');
+        done();
+      });
+    });
+  });
 
-	after(function(done) {
-		User.remove().exec();
-		done();
-	});
+  after(function(done) {
+    User.remove().exec();
+    done();
+  });
 });
