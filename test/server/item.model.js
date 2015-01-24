9f351063a6ac8406b582a0fa901ac8de8cbfb1ea
Ilan Biala <ilan.biala@gmail.com>
2015-01-23 23:17:23 -0500

    Better model assertions in tests

--- test/server/item.model.js
+++ test/server/item.model.js
@@ -2,45 +2,48 @@
  * Module dependencies
  */
 var should = require('should'),
-	mongoose = require('mongoose'),
-	Item = mongoose.model('Item');
+  mongoose = require('mongoose'),
+  Item = mongoose.model('Item');
 
 /**
  * Unit tests
  */
 describe('Item model unit tests:', function() {
-	before(function(done) {
-		done();
-	});
+  before(function(done) {
+    done();
+  });
 
-	describe('Creating an Item', function() {
-		it('should create and save an Item', function(done) {
-			var item = new Item({
-				title: 'A delicious test item',
-				description: 'A description',
-				price: 4.5,
-				active: true
-			});
+  describe('Creating an Item', function() {
+    it('should create and save an Item', function(done) {
+      var item = new Item({
+        title: 'A delicious test item',
+        description: 'A description',
+        price: 4.5,
+        active: true
+      });
 
-			item.save(done);
-		});
+      item.save(done);
+    });
 
-		it('fails to create an Item with missing field', function(done) {
-			var item = new Item({
-				description: 'A description',
-				price: 4.5,
-				active: true
-			});
+    it('fails to create an Item with missing field', function(done) {
+      var item = new Item({
+        // title missing
+        description: 'A description',
+        price: 4.5,
+        active: true
+      });
 
-			item.save(function(err) {
-				(err !== null).should.be.true;
-				done();
-			});
-		});
-	});
+      item.save(function(err) {
+        (err !== null).should.be.true;
+        err.message.should.equal('Validation failed');
+        err.errors.title.message.should.equal('Path `title` is required.');
+        done();
+      });
+    });
+  });
 
-	after(function(done) {
-		Item.remove().exec();
-		done();
-	});
+  after(function(done) {
+    Item.remove().exec();
+    done();
+  });
 });
