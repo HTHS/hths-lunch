4fdb22dba7da80de25df214fdf199d66c525d13f
Ilan Biala <ilan.biala@gmail.com>
2015-01-24 22:43:00 -0500

    Changed routes for panel inviting user, made use of id middleware.

--- app/controllers/panel.js
+++ app/controllers/panel.js
@@ -2,156 +2,156 @@
  * Module dependencies
  */
 var mongoose = require('mongoose'),
-	Item = mongoose.model('Item'),
-	Email = require('./email'),
-	item = require('./item'),
-	order = require('./order'),
-	schedule = require('./schedule'),
-	analytics = require('./analytics'),
-	user = require('./user'),
-	errorHandler = require('./error');
+  Item = mongoose.model('Item'),
+  Email = require('./email'),
+  item = require('./item'),
+  order = require('./order'),
+  schedule = require('./schedule'),
+  analytics = require('./analytics'),
+  user = require('./user'),
+  errorHandler = require('./error');
 
 exports.getItems = function(req, res) {
-	item.list(req, res);
+  item.list(req, res);
 };
 
 exports.createItem = function(req, res) {
-	item.create(req, res);
+  item.create(req, res);
 };
 
 exports.getItem = function(req, res) {
-	item.read(req, res);
+  item.read(req, res);
 };
 
 exports.updateItem = function(req, res) {
-	item.update(req, res);
+  item.update(req, res);
 };
 
 exports.deleteItem = function(req, res) {
-	item.delete(req, res);
+  item.delete(req, res);
 };
 
 exports.getOrders = function(req, res) {
-	order.list(req, res);
+  order.list(req, res);
 };
 
 exports.getOrder = function(req, res) {
-	order.read(req, res);
+  order.read(req, res);
 };
 
 exports.deleteOrder = function(req, res) {
-	order.delete(req, res);
+  order.delete(req, res);
 };
 
 exports.createSchedule = function(req, res) {
-	schedule.create(req, res);
+  schedule.create(req, res);
 };
 
 exports.getSchedule = function(req, res) {
-	schedule.read(req, res);
+  schedule.read(req, res);
 };
 
 exports.updateSchedule = function(req, res) {
-	schedule.update(req, res);
+  schedule.update(req, res);
 };
 
 exports.getTopItems = function(req, res) {
-	analytics.topItems(req, res);
+  analytics.topItems(req, res);
 };
 
 exports.getDays = function(req, res) {
-	analytics.getDays(req, res);
+  analytics.getDays(req, res);
 };
 
 exports.getUsers = function(req, res) {
-	user.list(req, res);
+  user.list(req, res);
 };
 
 exports.deleteUser = function(req, res) {
-	user.delete(req, res);
+  user.delete(req, res);
 };
 
 exports.inviteUser = function(req, res) {
-	var url = req.headers.origin + '/auth/google';
-	var isAdmin = req.body.isAdmin || false;
+  var url = req.headers.origin + '/auth/google';
+  var isAdmin = req.profile.isAdmin || false;
 
-	var stateParams = encodeURIComponent(JSON.stringify({
-		isAdmin: isAdmin
-	}));
-	url += '?state=' + stateParams;
+  var stateParams = encodeURIComponent(JSON.stringify({
+    isAdmin: isAdmin
+  }));
+  url += '?state=' + stateParams;
 
-	var options = {
-		to: req.body.email,
-		subject: 'Join HTHS-Lunch',
-		text: 'Join HTHS-Lunch (' + url +
-			') and start ordering lunch the right way.',
-		html: 'Join <a href="' + url +
-			'">HTHS-Lunch</a> and start ordering lunch the right way.'
-	};
+  var options = {
+    to: req.profile.email,
+    subject: 'Join HTHS-Lunch',
+    text: 'Join HTHS-Lunch (' + url +
+      ') and start ordering lunch the right way.',
+    html: 'Join <a href="' + url +
+      '">HTHS-Lunch</a> and start ordering lunch the right way.'
+  };
 
-	var email = new Email(options);
-	email
-		.send()
-		.then(function(info) {
-			user
-				.createPlaceholder(req.body.email, 'Invited')
-				.then(function(user) {
-					res.json(user);
-				})
-				.catch(function(err) {
-					res.status(400).json({
-						success: true,
-						user: user,
-						error: err
-					});
-				});
-		})
-		.catch(function(err) {
-			res.status(500).json({
-				success: false,
-				error: err
-			});
-		});
+  var email = new Email(options);
+  email
+    .send()
+    .then(function(info) {
+      user
+        .createPlaceholder(req.profile.email, 'Invited')
+        .then(function(user) {
+          res.json(user);
+        })
+        .catch(function(err) {
+          res.status(400).json({
+            success: true,
+            user: user,
+            error: err
+          });
+        });
+    })
+    .catch(function(err) {
+      res.status(500).json({
+        success: false,
+        error: err
+      });
+    });
 };
 
 /**
  * Authorization check
  */
 exports.userHasAuthorization = function(req, res, next) {
-	user.hasAuthorization(req, res);
+  user.hasAuthorization(req, res);
 };
 
 /**
  * Item middleware
  */
 exports.itemByID = function(req, res, next, id) {
-	item.itemByID(req, res, next, id);
+  item.itemByID(req, res, next, id);
 };
 
 /**
  * Order middleware
  */
 exports.orderByID = function(req, res, next, id) {
-	order.orderByID(req, res, next, id);
+  order.orderByID(req, res, next, id);
 };
 
 /**
  * User middleware
  */
 exports.userByID = function(req, res, next, id) {
-	user.userByID(req, res, next, id);
+  user.userByID(req, res, next, id);
 };
 
 /**
  * Require login middleware
  */
 exports.requiresLogin = function(req, res, next) {
-	user.requiresLogin(req, res, next);
+  user.requiresLogin(req, res, next);
 };
 
 /**
  * Panel authorization middleware
  */
 exports.requiresAuthentication = function(req, res, next) {
-	user.requiresAuthentication(req, res, next);
+  user.requiresAuthentication(req, res, next);
 };
--- app/routes/panel.js
+++ app/routes/panel.js
@@ -1,56 +1,56 @@
 var router = require('express').Router(),
-	panel = require('../controllers/panel');
+  panel = require('../controllers/panel');
 
 router.all('*', panel.requiresLogin, panel.requiresAuthentication);
 
 // Item Routes
 router.route('/items')
-	.get(panel.getItems)
-	.post(panel.createItem);
+  .get(panel.getItems)
+  .post(panel.createItem);
 
 router.route('/items/:itemId')
-	.get(panel.getItem)
-	.put(panel.updateItem)
-	.delete(panel.deleteItem);
+  .get(panel.getItem)
+  .put(panel.updateItem)
+  .delete(panel.deleteItem);
 
 // Lunch order routes
 router.route('/orders')
-	.get(panel.getOrders);
+  .get(panel.getOrders);
 
 router.route('/orders/:orderId')
-	.get(panel.getOrder)
-	.delete(panel.deleteOrder);
+  .get(panel.getOrder)
+  .delete(panel.deleteOrder);
 
 // Schedule routes
 router.route('/schedule')
-	.get(panel.getSchedule)
-	.post(panel.createSchedule)
-	.put(panel.updateSchedule);
+  .get(panel.getSchedule)
+  .post(panel.createSchedule)
+  .put(panel.updateSchedule);
 
 // Analytics routes
 router.route('/analytics')
-	.get(panel.getTopItems);
+  .get(panel.getTopItems);
 
 router.route('/analytics/top-items')
-	.get(panel.getTopItems);
+  .get(panel.getTopItems);
 
 router.route('/analytics/days')
-	.get(panel.getDays);
+  .get(panel.getDays);
 
 // Users routes
 router.route('/users')
-	.get(panel.getUsers)
-	.post(panel.inviteUser);
+  .get(panel.getUsers);
 
 router.route('/users/:userId')
-	.delete(panel.deleteUser);
+  .post(panel.inviteUser)
+  .delete(panel.deleteUser);
 
 router.route('/auth/:userId')
-	.post(panel.userHasAuthorization);
+  .post(panel.userHasAuthorization);
 
 router.param('itemId', panel.itemByID)
-	.param('orderId', panel.orderByID)
-	.param('userId', panel.userByID);
+  .param('orderId', panel.orderByID)
+  .param('userId', panel.userByID);
 
 module.exports.basePath = '/api/panel';
 module.exports.router = router;
