f0620a8fc19d46e947d7c2cc231d4a04b2e6ba49
Ilan Biala <ilan.biala@gmail.com>
2015-01-26 17:03:17 -0500

    Use Database service to check if user is logged in.

--- public/modules/core/services/database-service.js
+++ public/modules/core/services/database-service.js
@@ -1,28 +1,34 @@
 angular.module('hthsLunch.core.databaseService', ['hthsLunch.core.itemService', 'hthsLunch.core.orderService',
-    'hthsLunch.core.userService', 'hthsLunch.core.authService'
-  ])
-  .factory('Database', ['$q', 'Item', 'Order', 'User', 'Auth', function($q, Item, Order, User, Auth) {
-    var service = {};
+		'hthsLunch.core.userService', 'hthsLunch.core.authService'
+	])
+	.factory('Database', ['$q', 'Item', 'Order', 'User', 'Auth', function($q, Item, Order, User, Auth) {
+		var service = {};
 
-    var me;
+		var me;
 
-    service.getMe = function() {
-      return me;
-    };
+		service.getMe = function() {
+			return me;
+		};
 
-    service.fetchMe = function() {
-      var query = User.me().$promise;
+		service.fetchMe = function() {
+			var promise = User.me().$promise;
 
-      query.then(function(user) {
-        me = user;
-      });
+			promise
+				.then(function(user) {
+					me = user;
+				})
+				.catch(function(response) {
+					if (response.status === 401) {
+						me = null;
+					}
+				});
 
-      return query;
-    };
+			return promise;
+		};
 
-    service.fetchAll = function() {
-      return $q.all([this.fetchMe()]);
-    };
+		service.fetchAll = function() {
+			return $q.all([this.fetchMe()]);
+		};
 
-    return service;
-  }]);
+		return service;
+	}]);
