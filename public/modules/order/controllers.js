0c71628d01a66a088c0a077ee31fe5ca8f5b29b7
Ilan Biala <ilan.biala@gmail.com>
2015-01-26 21:37:03 -0500

    Change item.quantity so form looks nicer

--- public/modules/order/controllers.js
+++ public/modules/order/controllers.js
@@ -15,7 +15,7 @@ angular.module('hthsLunch.order').controller('OrderController', ['$scope', '$sta
 				.query()
 				.$promise.then(function(items) {
 					$scope.menu = items.map(function(item) {
-						item.quantity = 0;
+						// item.quantity = 0; // setting this will cause ng-model to show quantity 0 in form
 						return item;
 					});
 
@@ -28,20 +28,20 @@ angular.module('hthsLunch.order').controller('OrderController', ['$scope', '$sta
 
 		function populateForm(order) {
 			for (var i = 0; i < order.items.length; i++) {
-								for (var z = 0; z < $scope.menu.length; z++) {
+				for (var z = 0; z < $scope.menu.length; z++) {
 					if (order.items[i] === $scope.menu[z]._id) {
 						$scope.menu[z].quantity = order.quantity[i];
-										$scope.toggleItemInOrder(z);
-									}
-								}
-							}
+						$scope.toggleItemInOrder(z);
+					}
+				}
+			}
 
 			if (order.toUpdate) {
 				$scope.newOrder._id = order._id;
 				$scope.newOrder.total = order.total;
 				$scope.newOrder.toBeUpdated = true;
-						}
-					}
+			}
+		}
 
 		$scope.itemInOrder = function(index) {
 			return $scope.newOrder.items[index] !== null;
@@ -51,7 +51,7 @@ angular.module('hthsLunch.order').controller('OrderController', ['$scope', '$sta
 			if ($scope.newOrder.items[index] !== null && typeof $scope.newOrder.items[
 					index] === 'object') {
 				delete $scope.newOrder.items[index];
-				$scope.menu[index].quantity = 0;
+				$scope.menu[index].quantity = null;
 			} else {
 				$scope.newOrder.items[index] = $scope.menu[index];
 				$scope.menu[index].quantity = 1;
@@ -61,7 +61,7 @@ angular.module('hthsLunch.order').controller('OrderController', ['$scope', '$sta
 
 		$scope.checkIfBlank = function(index) {
 			if ($scope.menu[index].quantity === undefined) {
-				$scope.menu[index].quantity = 0;
+				$scope.menu[index].quantity = null;
 			}
 		}
 
@@ -76,14 +76,12 @@ angular.module('hthsLunch.order').controller('OrderController', ['$scope', '$sta
 
 		$scope.submitOrder = function() {
 			if ($scope.newOrder.customer && $scope.newOrder.total > 0) {
+				$scope.newOrder.quantity = [];
 				$scope.newOrder.items = $scope.menu.slice(0); // easy way to clone an Array by value
+
 				$scope.newOrder.items = $scope.newOrder.items.filter(function(item) {
 					return item.quantity > 0;
-				});
-
-				$scope.newOrder.quantity = [];
-
-				$scope.newOrder.items = $scope.newOrder.items.map(function(item) {
+				}).map(function(item) {
 					$scope.newOrder.quantity.push(item.quantity);
 					return item._id;
 				});
