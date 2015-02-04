d43359783fa2fa08c02846431101a1f4378b2354
Ilan Biala <ilan.biala@gmail.com>
2015-02-04 16:02:59 -0500

    The start of the passport mock logic

--- test/server/mock-strategy.js
+++ test/server/mock-strategy.js
@@ -1,35 +1,40 @@
 var passport = require('passport'),
-	util = require('util');
+  util = require('util');
 
-	function MockStrategy(options, verify) {
-		this.name = 'mock';
-		this.passAuthentication = options.passAuthentication || true;
-		this.userId = options.userId || 1;
-		this.verify = verify;
-	}
+function MockStrategy(options, verify) {
+  this.name = 'mock';
+  this.passAuthentication = options.passAuthentication || true;
+  this.userId = options.userId || 1;
+  this.verify = verify;
+}
 
-	util.inherits(MockStrategy, passport.Strategy);
+util.inherits(MockStrategy, passport.Strategy);
 
-	MockStrategy.prototype.authenticate = function authenticate(req) {
-		if (this.passAuthentication) {
-			var user = {
-					id: this.userId
-				};
-			var self = this;
-			this.verify(user, function(err, user) {
-				if(err) {
-					self.fail(err);
-				} else {
-					self.success(user);
-				}
-			});
-		} else {
-			this.fail('Unauthorized');
-		}
-	};
+MockStrategy.prototype.authenticate = function authenticate(req) {
+  if (this.passAuthentication) {
+    var user = {
+      id: this.userId
+    };
+    var self = this;
+    this.verify(user, function(err, user) {
+      if (err) {
+        self.fail(err);
+      } else {
+        self.success(user);
+      }
+    });
+  } else {
+    this.fail('Unauthorized');
+  }
+};
 
 module.exports = function(app, options) {
-	// create your verify function on your own -- should do similar things as the "real" one.
-	passport.use(new MockStrategy(options, verifyFunction));
-	app.get('/auth/mock', passport.authenticate('mock'));
+  // create your verify function on your own -- should do similar things as the "real" one.
+  passport.use(new MockStrategy({
+    callbackURL: '/auth/mock'
+  }, function(req, accessToken, refreshToken, profile, done) {
+    console.log(arguments);
+  }));
+
+  app.get('/auth/mock', passport.authenticate('mock'));
 };
