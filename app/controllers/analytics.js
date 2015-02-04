1f0982897d7349f0e668ac26b0f056ac51bf8c3c
Ilan Biala <ilan.biala@gmail.com>
2015-02-04 15:34:26 -0500

    Send the days in reverse order to get them properly on the frontend

--- app/controllers/analytics.js
+++ app/controllers/analytics.js
@@ -2,44 +2,44 @@
  * Module dependencies
  */
 var mongoose = require('mongoose'),
-  Item = mongoose.model('Item'),
-  Day = mongoose.model('Day'),
-  errorHandler = require('./error');
+	Item = mongoose.model('Item'),
+	Day = mongoose.model('Day'),
+	errorHandler = require('./error');
 
 /**
  * Order items by number of ordered
  */
 exports.topItems = function(req, res) {
-  Item
-    .find()
-    .sort('-numberOrdered')
-    .sort('title')
-    .exec(function(err, items) {
-      if (err) {
-        return res.status(400).send({
-          message: errorHandler.getErrorMessage(err)
-        });
-      } else {
-        res.json(items);
-      }
-    });
+	Item
+		.find()
+		.sort('-numberOrdered')
+		.sort('title')
+		.exec(function(err, items) {
+			if (err) {
+				return res.status(400).send({
+					message: errorHandler.getErrorMessage(err)
+				});
+			} else {
+				res.json(items);
+			}
+		});
 };
 
 /**
  * Get last 10 days
  */
 exports.getDays = function(req, res) {
-  Day
-    .find()
-    .sort('-date')
-    .limit(10)
-    .exec(function(err, days) {
-      if (err) {
-        return res.status(400).send({
-          message: errorHandler.getErrorMessage(err)
-        });
-      } else {
-        res.json(days);
-      }
-    });
+	Day
+		.find()
+		.sort('-date')
+		.limit(10)
+		.exec(function(err, days) {
+			if (err) {
+				return res.status(400).send({
+					message: errorHandler.getErrorMessage(err)
+				});
+			} else {
+				res.json(days.reverse());
+			}
+		});
 };
