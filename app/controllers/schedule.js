3e88f122bb28e46eabd5e758073cf6318a000507
Ilan Biala <ilan.biala@gmail.com>
2015-02-05 19:40:12 -0500

    Add raw schedule request path to schedule controller

--- app/controllers/schedule.js
+++ app/controllers/schedule.js
@@ -2,16 +2,16 @@
  * Module dependencies.
  */
 var _ = require('lodash'),
-  later = require('later'),
-  mongoose = require('mongoose'),
-  Promise = require('bluebird'),
-  Schedule = mongoose.model('Schedule'),
-  Order = mongoose.model('Order'),
-  Item = mongoose.model('Item'),
-  Day = mongoose.model('Day'),
-  csv = require('./csv'),
-  Email = require('./email'),
-  errorHandler = require('./error');
+	later = require('later'),
+	mongoose = require('mongoose'),
+	Promise = require('bluebird'),
+	Schedule = mongoose.model('Schedule'),
+	Order = mongoose.model('Order'),
+	Item = mongoose.model('Item'),
+	Day = mongoose.model('Day'),
+	csv = require('./csv'),
+	Email = require('./email'),
+	errorHandler = require('./error');
 
 // Use ET for calculating start end days of school year
 later.date.localTime();
@@ -20,340 +20,347 @@ later.date.localTime();
  * Create a Schedule
  */
 exports.create = function(req, res) {
-  var startDate = new Date(req.body.startDate);
-  var endDate = new Date(req.body.endDate);
-  var time = req.body.time;
-  var exceptions = req.body.exceptions;
+	var startDate = new Date(req.body.startDate);
+	var endDate = new Date(req.body.endDate);
+	var time = req.body.time;
+	var exceptions = req.body.exceptions;
 
-  exports.schoolDays = later.parse.recur()
-    .on(time).hour()
-    .onWeekday()
-    .on(1, 2, 3, 4, 5, 6, 9, 10, 11, 12).month();
+	exports.schoolDays = later.parse.recur()
+		.on(time).hour()
+		.onWeekday()
+		.on(1, 2, 3, 4, 5, 6, 9, 10, 11, 12).month();
 
-  exceptions.forEach(function(exception, index) {
-    exception = new Date(exception);
+	exceptions.forEach(function(exception, index) {
+		exception = new Date(exception);
 
-    if (index === 0) {
-      exports.schoolDays.except()
-        .on(exception.getMonth()).month()
-        .on(exception.getDate()).dayOfMonth()
-        .on(exception.getFullYear()).year();
-    } else {
-      exports.schoolDays.and()
-        .on(exception.getMonth()).month()
-        .on(exception.getDate()).dayOfMonth()
-        .on(exception.getFullYear()).year();
-    }
-  });
+		if (index === 0) {
+			exports.schoolDays.except()
+				.on(exception.getMonth()).month()
+				.on(exception.getDate()).dayOfMonth()
+				.on(exception.getFullYear()).year();
+		} else {
+			exports.schoolDays.and()
+				.on(exception.getMonth()).month()
+				.on(exception.getDate()).dayOfMonth()
+				.on(exception.getFullYear()).year();
+		}
+	});
 
-  exports.schoolDays.startDate = startDate;
-  exports.schoolDays.endDate = endDate;
+	exports.schoolDays.startDate = startDate;
+	exports.schoolDays.endDate = endDate;
 
-  var schedule = new Schedule(exports.schoolDays);
+	var schedule = new Schedule(exports.schoolDays);
 
-  schedule.save(function(err) {
-    if (err) {
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      exports.schoolSchedule = later.schedule(exports.schoolDays)
-        .next(500, startDate, endDate);
+	schedule.save(function(err) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			exports.schoolSchedule = later.schedule(exports.schoolDays)
+				.next(500, startDate, endDate);
 
-      exports.job = later.setInterval(endSubmissionsForDay, exports.schoolDays);
+			exports.job = later.setInterval(endSubmissionsForDay, exports.schoolDays);
 
-      res.json(exports.schoolSchedule);
-    }
-  });
+			res.json(exports.schoolSchedule);
+		}
+	});
 };
 
 /**
  * Show the current Schedule
  */
 exports.read = function(req, res) {
-  res.json(exports.schoolSchedule);
+	res.json(exports.schoolSchedule);
+};
+
+/**
+ * Get the current schedule raw data
+ */
+exports.getRawSchedule = function(req, res) {
+	res.json(exports.schoolDays);
 };
 
 /**
  * Update a Schedule
  */
 exports.update = function(req, res) {
-  exports.schoolDays = _.union(exports.schoolDays, req.body.exceptions);
+	exports.schoolDays = _.union(exports.schoolDays, req.body.exceptions);
 
-  if (req.body.time) {
-    exports.schoolDays.schedules[0].h[0] = req.body.time;
-  }
+	if (req.body.time) {
+		exports.schoolDays.schedules[0].h[0] = req.body.time;
+	}
 
-  Schedule.findOneAndUpdate({}).exec(function(err, schedule) {
-    if (err) {
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      exports.schoolSchedule = later.schedule(exports.schoolDays)
-        .next(500, exports.schoolDays.startDate, exports.schoolDays.endDate);
+	Schedule.findOneAndUpdate({}).exec(function(err, schedule) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			exports.schoolSchedule = later.schedule(exports.schoolDays)
+				.next(500, exports.schoolDays.startDate, exports.schoolDays.endDate);
 
-      exports.job.clear();
-      exports.job = later.setInterval(endSubmissionsForDay, exports.schoolDays);
+			exports.job.clear();
+			exports.job = later.setInterval(endSubmissionsForDay, exports.schoolDays);
 
-      res.json(exports.schoolSchedule);
-    }
-  });
+			res.json(exports.schoolSchedule);
+		}
+	});
 };
 
 exports.init = function scheduleOrderProcessing() {
-  var p = Promise.defer();
+	var p = Promise.defer();
 
-  Schedule.findOne().exec(function(err, schedule) {
-    if (!err && schedule) {
-      var startDate = new Date(schedule.startDate);
-      var endDate = new Date(schedule.endDate);
-      var time = schedule.schedules[0].h[0];
-      var exceptions = schedule.exceptions;
+	Schedule.findOne().exec(function(err, schedule) {
+		if (!err && schedule) {
+			var startDate = new Date(schedule.startDate);
+			var endDate = new Date(schedule.endDate);
+			var time = schedule.schedules[0].h[0];
+			var exceptions = schedule.exceptions;
 
-      exports.schoolDays = later.parse.recur()
-        .on(time).hour()
-        .onWeekday()
-        .on(1, 2, 3, 4, 5, 6, 9, 10, 11, 12).month();
+			exports.schoolDays = later.parse.recur()
+				.on(time).hour()
+				.onWeekday()
+				.on(1, 2, 3, 4, 5, 6, 9, 10, 11, 12).month();
 
-      exceptions.forEach(function(exception, index) {
-        exception = new Date(exception);
+			exceptions.forEach(function(exception, index) {
+				exception = new Date(exception);
 
-        if (index === 0) {
-          exports.schoolDays.except()
-            .on(exception.getMonth()).month()
-            .on(exception.getDate()).dayOfMonth()
-            .on(exception.getFullYear()).year();
-        } else {
-          exports.schoolDays.and()
-            .on(exception.getMonth()).month()
-            .on(exception.getDate()).dayOfMonth()
-            .on(exception.getFullYear()).year();
-        }
-      });
+				if (index === 0) {
+					exports.schoolDays.except()
+						.on(exception.getMonth()).month()
+						.on(exception.getDate()).dayOfMonth()
+						.on(exception.getFullYear()).year();
+				} else {
+					exports.schoolDays.and()
+						.on(exception.getMonth()).month()
+						.on(exception.getDate()).dayOfMonth()
+						.on(exception.getFullYear()).year();
+				}
+			});
 
-      exports.schoolDays.startDate = startDate;
-      exports.schoolDays.endDate = endDate;
-      exports.schoolSchedule = later.schedule(exports.schoolDays).next(
-        500,
-        exports.schoolDays.startDate, exports.schoolDays.endDate);
-      exports.job = later.setInterval(endSubmissionsForDay, exports.schoolDays);
-    }
+			exports.schoolDays.startDate = startDate;
+			exports.schoolDays.endDate = endDate;
+			exports.schoolSchedule = later.schedule(exports.schoolDays).next(
+				500,
+				exports.schoolDays.startDate, exports.schoolDays.endDate);
+			exports.job = later.setInterval(endSubmissionsForDay, exports.schoolDays);
+		}
 
-    p.resolve();
-  });
+		p.resolve();
+	});
 
-  return p.promise;
+	return p.promise;
 };
 
 exports.isBetween = function(date) {
-  if (!exports.schoolDays) {
-    return false;
-  }
+	if (!exports.schoolDays) {
+		return false;
+	}
 
-  var now = new Date();
-  var prev1 = later.schedule(exports.schoolDays).prev(1, now);
-  var next1 = later.schedule(exports.schoolDays).next(1, now);
+	var now = new Date();
+	var prev1 = later.schedule(exports.schoolDays).prev(1, now);
+	var next1 = later.schedule(exports.schoolDays).next(1, now);
 
-  if (next1 - date < next1 - prev1) {
-    return true;
-  } else {
-    return false;
-  }
+	if (next1 - date < next1 - prev1) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 function findOrdersBetween(d1, d2) {
-  var p = Promise.defer();
+	var p = Promise.defer();
 
-  Order
-    .find({})
-    .where('created').gte(d1).lte(d2)
-    .sort('created')
-    .populate('items')
-    .exec(function(err, orders) {
-      if (err) {
-        p.reject(err);
-      } else {
-        p.resolve(orders);
-      }
-    });
+	Order
+		.find({})
+		.where('created').gte(d1).lte(d2)
+		.sort('created')
+		.populate('items')
+		.exec(function(err, orders) {
+			if (err) {
+				p.reject(err);
+			} else {
+				p.resolve(orders);
+			}
+		});
 
-  return p.promise;
+	return p.promise;
 }
 
 function createDay(orders) {
-  var p = Promise.defer();
+	var p = Promise.defer();
 
-  var today;
+	var today;
 
-  if (orders.length) {
-    orders.forEach(function(order) {
-      order.items.forEach(function(item) {
-        item
-          .update({
-            $inc: {
-              numberOrdered: 1
-            }
-          }, {}, function(err, numberUpdated, result) {
-            if (err) {
-              console.error('Error: Tried to increment numberOrdered on item: ', item);
-              console.error(err);
-            }
-          });
-      });
-    });
+	if (orders.length) {
+		orders.forEach(function(order) {
+			order.items.forEach(function(item) {
+				item
+					.update({
+						$inc: {
+							numberOrdered: 1
+						}
+					}, {}, function(err, numberUpdated, result) {
+						if (err) {
+							console.error('Error: Tried to increment numberOrdered on item: ', item);
+							console.error(err);
+						}
+					});
+			});
+		});
 
-    var orderIDs = orders.map(function(order) {
-      return order._id;
-    });
+		var orderIDs = orders.map(function(order) {
+			return order._id;
+		});
 
-    today = new Day({
-      orders: orderIDs
-    });
-  } else {
-    today = new Day({
-      orders: []
-    });
-  }
+		today = new Day({
+			orders: orderIDs
+		});
+	} else {
+		today = new Day({
+			orders: []
+		});
+	}
 
-  today.save(function(err, today) {
-    if (err) {
-      p.reject(err);
-      console.error(err);
-    } else {
-      p.resolve(today);
-    }
-  });
+	today.save(function(err, today) {
+		if (err) {
+			p.reject(err);
+			console.error(err);
+		} else {
+			p.resolve(today);
+		}
+	});
 
-  return p.promise;
+	return p.promise;
 }
 
 function createCSVInput(today) {
-  var orderData = {
-    items: {},
-    quantity: [],
-    total: 0
-  };
+	var orderData = {
+		items: {},
+		quantity: [],
+		total: 0
+	};
 
-  var orderCSVData = [
-    ['Items', 'Quantity', 'Total']
-  ];
-  var customerCSVData = [
-    ['Customer', 'Items', 'Total']
-  ];
+	var orderCSVData = [
+		['Items', 'Quantity', 'Total']
+	];
+	var customerCSVData = [
+		['Customer', 'Items', 'Total']
+	];
 
-  var orderTallyPromises = [];
-  today.orders.forEach(function(orderID) {
-    var p = Promise.defer();
-    orderTallyPromises.push(p.promise);
+	var orderTallyPromises = [];
+	today.orders.forEach(function(orderID) {
+		var p = Promise.defer();
+		orderTallyPromises.push(p.promise);
 
-    Order
-      .findById(orderID)
-      .populate('items')
-      .populate('user')
-      .exec(function(err, order) {
-        if (err) {
-          // TODO do something
-          p.reject(err);
-        } else {
-          order.items.forEach(function(item) {
-            if (orderData.items[item._id]) {
-              orderData.items[item._id].quantity++;
-            } else {
-              orderData.items[item._id] = {
-                title: item.title,
-                quantity: 1,
-                price: item.price
-              };
-            }
-          });
+		Order
+			.findById(orderID)
+			.populate('items')
+			.populate('user')
+			.exec(function(err, order) {
+				if (err) {
+					// TODO do something
+					p.reject(err);
+				} else {
+					order.items.forEach(function(item) {
+						if (orderData.items[item._id]) {
+							orderData.items[item._id].quantity++;
+						} else {
+							orderData.items[item._id] = {
+								title: item.title,
+								quantity: 1,
+								price: item.price
+							};
+						}
+					});
 
-          var user = order.user.displayName;
-          var itemsOrdered = order.items.map(function(item, index) {
-            return item.title + ' x' + order.quantity[index];
-          }).join(', ');
-          var total = order.total;
-          customerCSVData.push([user, itemsOrdered, total]);
+					var user = order.user.displayName;
+					var itemsOrdered = order.items.map(function(item, index) {
+						return item.title + ' x' + order.quantity[index];
+					}).join(', ');
+					var total = order.total;
+					customerCSVData.push([user, itemsOrdered, total]);
 
-          p.resolve(order);
-        }
-      });
-  });
+					p.resolve(order);
+				}
+			});
+	});
 
-  return Promise
-    .all(orderTallyPromises)
-    .then(function() {
-      orderData.items = Object.keys(orderData.items).map(function(key) {
-        return orderData.items[key];
-      });
+	return Promise
+		.all(orderTallyPromises)
+		.then(function() {
+			orderData.items = Object.keys(orderData.items).map(function(key) {
+				return orderData.items[key];
+			});
 
-      for (var i = 0; i < orderData.items.length; i++) {
-        var item = orderData.items[i];
-        var quantity = item.quantity;
-        var itemTotal = item.price * quantity;
-        orderCSVData.push([item.title, quantity, '$' + itemTotal]);
-        orderData.total += itemTotal;
-      }
-      orderCSVData.push([]);
-      orderCSVData.push(['Grand total:', '', '$' + orderData.total]);
+			for (var i = 0; i < orderData.items.length; i++) {
+				var item = orderData.items[i];
+				var quantity = item.quantity;
+				var itemTotal = item.price * quantity;
+				orderCSVData.push([item.title, quantity, '$' + itemTotal]);
+				orderData.total += itemTotal;
+			}
+			orderCSVData.push([]);
+			orderCSVData.push(['Grand total:', '', '$' + orderData.total]);
 
-      return Promise.join(csv.generate(orderCSVData).then(function(csv) {
-        return csv;
-      }), csv.generate(customerCSVData).then(function(csv) {
-        return csv;
-      }), function(orderCSV, customerCSV) {
-        return {
-          orderCSV: orderCSV,
-          customerCSV: customerCSV
-        };
-      });
-    });
+			return Promise.join(csv.generate(orderCSVData).then(function(csv) {
+				return csv;
+			}), csv.generate(customerCSVData).then(function(csv) {
+				return csv;
+			}), function(orderCSV, customerCSV) {
+				return {
+					orderCSV: orderCSV,
+					customerCSV: customerCSV
+				};
+			});
+		});
 }
 
 function emailCSV(csvContents) {
-  var today = new Date();
+	var today = new Date();
 
-  console.log('Today\'s CSVs:\n', csvContents);
+	console.log('Today\'s CSVs:\n', csvContents);
 
-  var options = {
-    to: 'ibiala@ctemc.org',
-    subject: 'HTHS-Lunch CSV',
-    text: 'Attached are the CSVs.',
-    html: 'Attached are the CSVs.',
-    attachments: [{
-      filename: 'HTHS-bcc-' + today.toDateString().replace(/\s/g, '-') + '.csv',
-      content: csvContents.orderCSV
-    }, {
-      filename: 'HTHS-orders-' + today.toDateString().replace(/\s/g, '-') + '.csv',
-      content: csvContents.customerCSV
-    }]
-  };
+	var options = {
+		to: 'ibiala@ctemc.org',
+		subject: 'HTHS-Lunch CSV',
+		text: 'Attached are the CSVs.',
+		html: 'Attached are the CSVs.',
+		attachments: [{
+			filename: 'HTHS-bcc-' + today.toDateString().replace(/\s/g, '-') + '.csv',
+			content: csvContents.orderCSV
+		}, {
+			filename: 'HTHS-orders-' + today.toDateString().replace(/\s/g, '-') + '.csv',
+			content: csvContents.customerCSV
+		}]
+	};
 
-  var email = new Email(options);
+	var email = new Email(options);
 
-  email
-    .send()
-    .then(function(info) {
-      console.log('Sent CSVs for ', today.toDateString());
-      console.log(info);
-    })
-    .catch(function(err) {
-      console.error(err);
-    });
+	email
+		.send()
+		.then(function(info) {
+			console.log('Sent CSVs for ', today.toDateString());
+			console.log(info);
+		})
+		.catch(function(err) {
+			console.error(err);
+		});
 }
 
 function endSubmissionsForDay() {
-  var today = new Date();
-  var yesterday = later.schedule(exports.schoolDays).prev(2, today)[1];
-  yesterday.setSeconds(1);
+	var today = new Date();
+	var yesterday = later.schedule(exports.schoolDays).prev(2, today)[1];
+	yesterday.setSeconds(1);
 
-  console.log('Ending submissions for this period from %s to %s', yesterday.toUTCString(), today.toUTCString());
+	console.log('Ending submissions for this period from %s to %s', yesterday.toUTCString(), today.toUTCString());
 
-  findOrdersBetween(yesterday, today)
-    .then(createDay)
-    .then(createCSVInput)
-    .then(emailCSV)
-    .catch(function(err) {
-      console.error('Error: ', err);
-    });
+	findOrdersBetween(yesterday, today)
+		.then(createDay)
+		.then(createCSVInput)
+		.then(emailCSV)
+		.catch(function(err) {
+			console.error('Error: ', err);
+		});
 }
--- app/routes/panel.js
+++ app/routes/panel.js
@@ -1,59 +1,62 @@
 var router = require('express').Router(),
-	panel = require('../controllers/panel');
+  panel = require('../controllers/panel');
 
 router.all('*', panel.requiresLogin, panel.requiresAuthentication);
 
 // Item Routes
 router.route('/items')
-	.get(panel.getItems)
-	.post(panel.createItem);
+  .get(panel.getItems)
+  .post(panel.createItem);
 
 router.route('/items/:itemId')
-	.get(panel.getItem)
-	.put(panel.updateItem)
-	.delete(panel.deleteItem);
+  .get(panel.getItem)
+  .put(panel.updateItem)
+  .delete(panel.deleteItem);
 
 // Lunch order routes
 router.route('/orders')
-	.get(panel.getOrders);
+  .get(panel.getOrders);
 
 router.route('/orders/:orderId')
-	.get(panel.getOrder)
-	.delete(panel.deleteOrder);
+  .get(panel.getOrder)
+  .delete(panel.deleteOrder);
 
 // Schedule routes
 router.route('/schedule')
-	.get(panel.getSchedule)
-	.post(panel.createSchedule)
-	.put(panel.updateSchedule);
+  .get(panel.getSchedule)
+  .post(panel.createSchedule)
+  .put(panel.updateSchedule);
+
+router.route('/schedule/raw')
+  .get(panel.getRawSchedule);
 
 // Analytics routes
 router.route('/analytics')
-	.get(panel.getTopItems);
+  .get(panel.getTopItems);
 
 router.route('/analytics/top-items')
-	.get(panel.getTopItems);
+  .get(panel.getTopItems);
 
 router.route('/analytics/days')
-	.get(panel.getDays);
+  .get(panel.getDays);
 
 // Users routes
 router.route('/users')
-	.post(panel.inviteUser)
-	.get(panel.getUsers);
+  .post(panel.inviteUser)
+  .get(panel.getUsers);
 
 router.route('/users/bulk')
-	.post(panel.inviteBulkUsers);
+  .post(panel.inviteBulkUsers);
 
 router.route('/users/:userId')
-	.delete(panel.deleteUser);
+  .delete(panel.deleteUser);
 
 router.route('/auth/:userId')
-	.post(panel.userHasAuthorization);
+  .post(panel.userHasAuthorization);
 
 router.param('itemId', panel.itemByID)
-	.param('orderId', panel.orderByID)
-	.param('userId', panel.userByID);
+  .param('orderId', panel.orderByID)
+  .param('userId', panel.userByID);
 
 module.exports.basePath = '/api/panel';
 module.exports.router = router;
