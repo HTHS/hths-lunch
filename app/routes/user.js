06bd49b93ba05f840519a8a292fc6c541c28aae5
Ilan Biala <ilan.biala@gmail.com>
2015-02-12 18:42:02 -0500

    Fix formatting

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,8 +2,8 @@
  * Module dependencies.
  */
 var router = require('express').Router(),
-	passport = require('passport'),
-	users = require('../controllers/user');
+  passport = require('passport'),
+  users = require('../controllers/user');
 
 // User Routes
 
@@ -21,33 +21,32 @@ router.route('/api/auth/signout').get(users.requiresLogin, users.signout);
 // Google OAuth routes
 // "Signup" link
 router.route('/auth/google').get(function(req, res) {
-	(passport.authenticate('google', {
-		scope: [
-			'https://www.googleapis.com/auth/userinfo.profile',
-			'https://www.googleapis.com/auth/userinfo.email',
-			'https://www.googleapis.com/auth/plus.login'
-		],
-		state: req.query.state
-	}))(req, res);
+  (passport.authenticate('google', {
+    scope: [
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/plus.login'
+    ],
+    state: req.query.state
+  }))(req, res);
 });
 
 if (process.env.NODE_ENV === 'test') {
-	router.route('/auth/mock').get(
-		function(req, res, next) {
-			(passport.authenticate('mock', function(err, user, info) {
-				if (err) {
-					return next(err);
-				}
-				if (!user) {
-					return res.status(500).json({
-						success: false
-					});
-				}
-				res.json({
-					success: true
-				});
-			}))(req, res, next);
-		});
+  router.route('/auth/mock').get(function(req, res, next) {
+    (passport.authenticate('mock', function(err, user, info) {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return res.status(500).json({
+          success: false
+        });
+      }
+      res.json({
+        success: true
+      });
+    }))(req, res, next);
+  });
 }
 
 // "Callback/Signin" link
