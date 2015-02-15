20fcc5f5cc9adcc8bb09d7a68bbee9ac86a4b0af
Ilan Biala <ilan.biala@gmail.com>
2015-02-15 09:20:34 -0500

    Auth service test

---
+++ test/client/unit/services/auth-service.js
@@ -0,0 +1,27 @@
+describe('Unit: Testing Services', function() {
+
+	var $httpBackend;
+	var AuthService;
+
+	beforeEach(module('hthsLunch'));
+	beforeEach(inject(function(_$httpBackend_, _Auth_) {
+		$httpBackend = _$httpBackend_;
+		AuthService = _Auth_;
+	}));
+
+	it('should be able to signout', function() {
+		$httpBackend
+			.expectGET('/api/auth/signout')
+			.respond({
+				success: true
+			});
+
+		var result = AuthService.signout();
+
+		$httpBackend.flush();
+
+		expect(angular.isObject(result)).toEqual(true);
+		expect(result.success).toEqual(true);
+	});
+
+});
